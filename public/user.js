bf61df923bc3e2e7fbffd605d5f32d3ad8350858
Leon180 <lioung860302@gmail.com>
2023-03-25 18:17:14 +0800

    feat: add avatar's editor

--- public/user.js
+++ public/user.js
@@ -1,9 +1,13 @@
 const editUserBtn = document.querySelector('.edit-user-btn')
-const cusModalContainer = document.querySelector('.cus-modal-container')
-const modalClose = document.querySelector('.modal-close')
+const editModalContainer = document.querySelector('.edit-container')
+const editModalClose = document.querySelector('.edit-modal-close')
+// cropper
+const avatarModalContainer = document.querySelector('.avatar-cropper-container')
+const avatarModalClose = document.querySelector('.avatar-modal-close')
 const upload_avatar_img = document.querySelector('#upload_avatar_img')
+const avatarCropperSubmit = document.querySelector('.avatar-cropper-submit')
 const upload_coverage_img = document.querySelector('#upload_coverage_img')
-const image = document.querySelector('#tailoringImg')
+const avatarimageCropField = document.querySelector('#avatarTailoringImg')
 editUserBtn.addEventListener('click', function getUserDataRenderPage() {
   // axios.get('/api/users/1')
   //   .then(function (response) {
@@ -18,41 +22,61 @@ editUserBtn.addEventListener('click', function getUserDataRenderPage() {
   //     // 3.always executed
   //   })
   console.log('click')
-  fadeIn(cusModalContainer, 'flex')
+  fadeIn(editModalContainer, 'flex')
   blockScroll()
 })
 
-modalClose.addEventListener('click', function closeUserData() {
-  fadeOut(cusModalContainer)
+editModalClose.addEventListener('click', function closeUserData() {
+  fadeOut(editModalContainer)
   unblockScroll()
 })
 
+// Cropper.js
+const crop = new Cropper(avatarimageCropField, {
+  viewMode: 3,
+  aspectRatio: 1,  // 預設比例
+  preview: '#previewImg',  // 預覽檢視
+  guides: false,   // 裁剪框的虛線(九宮格)
+  autoCropArea: 0.5, // 0-1之間的數值，定義自動剪裁區域的大小，預設0.8
+  dragMode: 'move', // 拖曳模式 crop(Default,新增裁剪框) / move(移動裁剪框&圖片) / none(無動作)
+  cropBoxResizable: true, // 是否有裁剪框調整四邊八點
+  movable: true, // 是否允許移動圖片
+  zoomable: true, // 是否允許縮放圖片大小
+  rotatable: false,   // 是否允許旋轉圖片
+  zoomOnWheel: true, // 是否允許通過滑鼠滾輪來縮放圖片
+  zoomOnTouch: true, // 是否允許通過觸控移動來縮放圖片
+  ready: function (e) {
+    console.log('ready!');
+  }
+})
 upload_avatar_img.addEventListener('change', function avatarImageToCropper() {
   let file = this.files[0]
   if (file) {
     let reader = new FileReader()
     reader.onload = function (e) {
-      let img = e.target.result
-      console.log(img)
-      image.src = img
-      // const crop = new Cropper(image, {
-      //   aspectRatio: 16 / 9,
-      //   crop(e) {
-      //     console.log(e.detail.x)
-      //     console.log(e.detail.y)
-      //     console.log(e.detail.width)
-      //     console.log(e.detail.height)
-      //     console.log(e.detail.rotate)
-      //     console.log(e.detail.scaleX)
-      //     console.log(e.detail.scaleY)
-      //   },
-      // })
-      console.log('d')
+      let imgSrc = e.target.result
+      console.log(imgSrc)
+      avatarimageCropField.src = imgSrc
+      crop.replace(imgSrc, false)
+      fadeIn(avatarModalContainer, 'flex')
+    }
+    reader.onerror = function () {
+      console.log('error')
     }
     reader.readAsDataURL(file)
-
   }
 })
+
+avatarModalClose.addEventListener('click', function closeAvatarCropper() {
+  fadeOut(avatarModalContainer)
+})
+
+avatarCropperSubmit.addEventListener('click', function submitAvatarCropper() {
+  const imgd = crop.getImageData()
+  console.log(imgd)
+  fadeOut(avatarModalContainer)
+})
+
 
 // fadeIn,...
 function fadeIn(element, display, duration = 300) {
