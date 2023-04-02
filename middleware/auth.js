d84df0c8b23dc1c1323395473e57f4062a66b524
zephyr Lin <zephyrlin33@gmail.com>
2023-04-02 13:55:44 +0800

    hbs file modify

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,7 +2,9 @@ const { ensureAuthenticated, getUser } = require('../_helpers.js')
 const authenticated = (req, res, next) => {
   // if (req.isAuthenticated)
   if (ensureAuthenticated(req)) {
-    return next()
+    if (getUser(req).role=="regular") return next()
+    res.redirect('/login')
+    
   }
   res.redirect('/login')
 }
@@ -10,7 +12,7 @@ const authenticatedAdmin = (req, res, next) => {
   // if (req.isAuthenticated)
   if (ensureAuthenticated(req)) {
     if (getUser(req).role=="admin") return next()
-    res.redirect('/')
+    res.redirect('/admin')
   } else {
     res.redirect('/admin')
   }
